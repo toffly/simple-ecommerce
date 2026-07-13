7d6252cfdf36dcd39ff2534e2f1bbfcf30d19ee0
toffly <toffly@outlook.com>
2026-07-13 12:47:17 +0700

    Fetch cart items from backend and pass to HomePage component

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -3,12 +3,22 @@ import { HomePage } from "./pages/HomePage/HomePage";
 import "./App.css";
 import { CheckoutPage } from "./pages/Checkout/CheckoutPage";
 import { OrderPage } from "./pages/OrderPage/OrderPage";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    axios.get("/api/cart-items").then((response) => {
+      setCart(response.data);
+    });
+  }, []);
+
   return (
     <Routes>
-      <Route index element={<HomePage />} />
-      <Route path="checkout" element={<CheckoutPage />} />
+      <Route index element={<HomePage cart={cart}/>} />
+      <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrderPage />} />
     </Routes>
   );
--- ecommerce-frontend/src/pages/HomePage/HomePage.jsx
+++ ecommerce-frontend/src/pages/HomePage/HomePage.jsx
@@ -3,9 +3,9 @@ import { Header } from "../../components/Header";
 import "./HomePage.css";
 import { useEffect, useState } from "react";
 
-export function HomePage() {
+export function HomePage({cart}) {
   const [products, setProducts] = useState([])
-  const [cart, setCart] = useState([])
+  
 
   // fetch('http://localhost:3000/api/products').then((response) => {
   //   response.json().then((data) => {
@@ -18,9 +18,7 @@ export function HomePage() {
       setProducts(response.data);
     });
 
-    axios.get("/api/cart-items").then((response) => {
-      setCart(response.data)
-    })
+    
   }, []);
 
   return (
