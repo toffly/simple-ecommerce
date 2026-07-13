3ed3d82ca9abb5d684bebd8e46192ac72831536b
toffly <toffly@outlook.com>
2026-07-13 12:00:47 +0700

    Refactor CheckoutPage and HomePage components; implement dynamic product rendering in HomePage

--- src/pages/Checkout/CheckoutPage.jsx
+++ src/pages/Checkout/CheckoutPage.jsx
@@ -1,5 +1,6 @@
 import './checkout-header.css'
 import './CheckoutPage.css'
+import { Link } from 'react-router';
 
 export function CheckoutPage() {
   return (
@@ -8,17 +9,17 @@ export function CheckoutPage() {
       <div className="checkout-header">
         <div className="header-content">
           <div className="checkout-header-left-section">
-            <a href="/">
-              <img className="logo" src="images/logo.png" />
+            <Link to="/">
+              <img className="logo" src="images/logo.png" />  
               <img className="mobile-logo" src="images/mobile-logo.png" />
-            </a>
+            </Link>
           </div>
 
           <div className="checkout-header-middle-section">
             Checkout (
-            <a className="return-to-home-link" href="/">
+            <Link className="return-to-home-link" to="/">
               3 items
-            </a>
+            </Link>
             )
           </div>
 
