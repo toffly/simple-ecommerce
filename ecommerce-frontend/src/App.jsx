a28637eef4172f0c95ab00de17eeae65669662bc
toffly <toffly@outlook.com>
2026-07-14 12:38:57 +0700

    Enhance OrderPage to display cart items and order details with proper formatting

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
     <Routes>
       <Route index element={<HomePage cart={cart}/>} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
-      <Route path="orders" element={<OrderPage />} />
+      <Route path="orders" element={<OrderPage cart={cart}/>} />
     </Routes>
   );
 }
