742740a3ad6b65e8cea17a04d89777c7150e7136
toffly <toffly@outlook.com>
2026-07-15 15:35:24 +0700

    Refactor App and HomePage components to integrate Product component for better modularity; implement addToCart functionality with quantity selection

--- ecommerce-frontend/src/App.jsx
+++ ecommerce-frontend/src/App.jsx
@@ -8,18 +8,18 @@ import axios from "axios";
 
 function App() {
   const [cart, setCart] = useState([]);
-
-  useEffect(() => {
-    const fetchAppData = async () => {
+  const loadCart = async () => {
       const response = await axios.get("/api/cart-items?expand=product"); //?expand=product is query parameter add additional info to the request
       setCart(response.data);
     };
-    fetchAppData()
+
+  useEffect(() => {
+    loadCart()
   }, []);
 
   return (
     <Routes>
-      <Route index element={<HomePage cart={cart} />} />
+      <Route index element={<HomePage cart={cart} loadCart={loadCart}/>} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrderPage cart={cart} />} />
     </Routes>
